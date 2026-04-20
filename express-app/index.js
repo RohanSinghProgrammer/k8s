c98075a5af712735181df012065cc34ee6be01cf
rohansingh-panini8 <rohan.singh@panini8.com>
2026-04-20 18:12:19 +0530

    created stateful sets

--- express-app/index.js
+++
@@ -1,8 +0,0 @@
-import express from "express"
-
-const app = express();
-
-app.get("/", (req, res) => res.send("Hello from express"))
-app.get("/health", (req, res) => res.status(200).json({ msg: "API is healthy", status: "OK" }))
-
-app.listen(4000, () => console.log("Application listen on port 4000"))
